26c985a861bd081d89e2a7a4818d433bf837212e
Elise <erbuzzell@gmail.com>
2021-04-03 14:00:17 -0500

    Initial commit

---
+++ CO2Mark1/ShowSplashScreenViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ShowSplashScreenViewController.swift
+//  CO2Mark1
+//
+//  Created by Elise Buzzell on 4/3/21.
+//
+
+import UIKit
+
+class ShowSplashScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
